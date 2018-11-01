5268591a0d92466046fd88638793413b46698149
Christian Cooper <christian.cooper@gmail.com>
2018-11-01 21:21:11 +0000

    Added main to run_me

--- run_me.py
+++ run_me.py
@@ -3,6 +3,9 @@ from data_extractor import build_question
 from cli import cli
 
 
-data = fetch_data("List of Hi-NRG artists and songs")
-questions = [build_question(data) for i in range(5)]
-cli(questions)
+if __name__ == '__main__':
+
+    data = fetch_data("List of Hi-NRG artists and songs")
+    questions = [build_question(data) for i in range(5)]
+    cli(questions)
+
